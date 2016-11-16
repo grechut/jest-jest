39ffcb1f267044a3a483bd508e2049bcff977f10
Jacek Rzeszutek <rzeszutekjacek@gmail.com>
2016-11-17 00:26:18 +0100

    Make error nicer in snapshot example

--- snapshots/format.js
+++ snapshots/format.js
@@ -1,6 +1,10 @@
 export function formatCurrency(currency) {
+    if (currency == null) { 
+        throw new Error('Wrong value');
+    }
+
     if (currency >= 1e6) {
-        return `$${(currency/1e6).toFixed(0)}m`
+        return `$${(currency / 1e6).toFixed(0)}m`
     }
     return `$${currency.toFixed(2)}`;
 }
