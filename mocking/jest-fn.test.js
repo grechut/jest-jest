95a6109dd9cf6d36867a901de8024244e555ba32
Jacek Rzeszutek <rzeszutekjacek@gmail.com>
2016-11-18 21:39:54 +0100

    Improve examples

--- mocking/jest-fn.test.js
+++ mocking/jest-fn.test.js
@@ -1,6 +1,11 @@
 test('Demo jest.fn(). Extra spying features.', () => {
-    const someFunc = jest.fn((actor) => `${actor} is good`);
+    const someFunc = jest.fn();
 
-    expect(someFunc('Brad Pitt')).toBe('Brad Pitt is good');
-    expect(someFunc).toHaveBeenLastCalledWith('Brad Pitt');
+    expect(someFunc()).toBe(undefined);
+    expect(someFunc).toBeCalled();
+
+    const someOtherFunc = jest.fn((actor) => `${actor} is good`);
+
+    expect(someOtherFunc('Brad Pitt')).toBe('Brad Pitt is good');
+    expect(someOtherFunc).toHaveBeenLastCalledWith('Brad Pitt');
 })
